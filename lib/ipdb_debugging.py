1b4fbde9544ab0faec541a00257fa840886529b9
waynetravis <waynetavis123@gmail.com>
2025-08-06 14:18:56 +0300

    commit debugging with ipdb

--- lib/ipdb_debugging.py
+++ lib/ipdb_debugging.py
@@ -1,7 +1,47 @@
 #!/usr/bin/env python3
 
+
 import ipdb
 
 def plus_two(num):
-    num + 2
-    return num
+ 
+    return num + 2
+
+def calculate_total(items):
+    
+    subtotal = sum(item['price'] for item in items)
+    ipdb.set_trace()  # Breakpoint here
+    tax = subtotal * 0.08
+    return subtotal + tax
+
+def process_data(data):
+   
+    ipdb.set_trace()
+    cleaned = data.strip()
+    ipdb.set_trace()
+    normalized = cleaned.lower()
+    ipdb.set_trace()
+    return normalized
+
+def format_name(first, last):
+   
+    full = f"{first} {last}"
+    ipdb.set_trace()
+    return full.title()
+
+def complex_calculation():
+  
+    try:
+        result = 10 / 0  # This will raise an error
+    except Exception:
+        ipdb.post_mortem()  # Debug after crash
+    return result
+
+def test_calculation():
+    
+    items = [{'price': 100}, {'price': 50}]
+    result = calculate_total(items)
+    ipdb.set_trace()
+    assert result == 162  # Expected result with 8% tax
+
+
